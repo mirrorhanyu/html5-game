482512c18fc17547bab5278a69593d4975457b1c
mirrorhanyu <mirrorhanyu@gmail.com>
2025-07-08 17:02:49 +0800

    introduce more games

--- app/_ui/home.tsx
+++ app/_ui/home.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import styles from "@/app/_ui/home.module.css";
 import Link from "next/link";
 import Image from "next/image";
 import { slugify } from "@/app/_util/slug";
@@ -46,24 +45,22 @@ export default function Home({ games }: HomeProps) {
 
   return (
     <main className="text-center mt-4">
-        <ResponsiveReactGridLayout
-          className="layout"
-          layouts={generateLayouts()}
-          cols={columnNumber}
-          rowHeight={30}
-          width={1200}
-        >
-          {games.map((game, index) => (
-            <div
-              key={index}
-              className={`text-center relative ${styles[`top${index + 1}`]}`}
-            >
-              <Link href={`/${slugify(game.title)}`}>
-                <Image src={game.img} alt={game.title} fill className="object-cover rounded-md" />
-              </Link>
-            </div>
-          ))}
-        </ResponsiveReactGridLayout>
+      <ResponsiveReactGridLayout
+        className="layout"
+        layouts={generateLayouts()}
+        cols={columnNumber}
+        rowHeight={30}
+        isDraggable={false}
+        isResizable={true}
+      >
+        {games.map((game, index) => (
+          <div key={index} className="text-center">
+            <Link href={`/${slugify(game.title)}`} className="w-full h-full block relative">
+              <Image src={game.img} alt={game.title} fill className="object-cover rounded-md" />
+            </Link>
+          </div>
+        ))}
+      </ResponsiveReactGridLayout>
     </main>
   )
 }
