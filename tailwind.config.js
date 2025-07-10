d74f45c410b5238347e2a9195ba92f116377398b
mirrorhanyu <mirrorhanyu@gmail.com>
2025-07-10 14:37:11 +0800

    add background

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
 // tailwind.config.js
-const {heroui} = require("@heroui/theme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./node_modules/@heroui/theme/dist/components/navbar.js",
-  ],
+  content: [],
   theme: {
     extend: {},
   },
   darkMode: "class",
-  plugins: [heroui()],
+  plugins: [],
 };
