6b79b83c343e0d124ed9f9bdd0cfda5a30b9217a
mirrorhanyu <mirrorhanyu@gmail.com>
2025-07-11 15:17:40 +0800

    add detail page

--- app/_ui/game.tsx
+++ app/_ui/game.tsx
@@ -6,17 +6,21 @@ export interface GameProps {
       src: string;
       img: string;
       from: string;
+      detail?: string;
     };
   }
 
 export default function Game({ game }: GameProps) {
   return (
-      <div>
+      <article className="prose">
           <h1>{game.title}</h1>
           <iframe
             src={`${game.src}`}
             className="w-214 h-117"
             allow="fullscreen; camera; focus-without-user-activation *; monetization; gamepad; keyboard-map *; xr-spatial-tracking; clipboard-write; web-share; accelerometer; magnetometer; gyroscope" />
-      </div>
+          {game.detail && (
+              <div className="mt-4" dangerouslySetInnerHTML={{ __html: game.detail }} />
+          )}
+      </article>
   )
 }
